fe907cef4b4595d3aac0cecd0186fd5d0a01a535
yorlan <yorlanfallas@gmail.com>
2024-09-11 19:10:01 -0600

    test de picha

--- tests/navigate.spec.ts
+++ tests/navigate.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test'
 
 test('navigate', async ({ page }) => {
-    await page.goto(process.env.URL);
+    //await page.goto(process.env.URL);
     //await page.goto('https://www.saucedemo.com/');
 });
